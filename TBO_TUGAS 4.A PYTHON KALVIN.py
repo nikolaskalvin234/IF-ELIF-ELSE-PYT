8d7a0d0ec5692ac5d86955a1ab1b9ad2cda8786d
nikolaskalvin <nikolaskalvin@gmail.com>
2024-08-09 19:17:31 +0700

    first commit

---
+++ TBO_TUGAS 4.A PYTHON KALVIN.py
@@ -0,0 +1,19 @@
+#.TUGAS 4.A - Kuliah TBO
+#.DOSEN = Arnes Y.V, M.Kom
+#. IF ELSE)
+print("NAMA = NIKOLAS KALVIN");
+print("NPM = 21421002");
+#
+#input data
+var_presiden = input("Presiden Pertama Indonesia : ")
+
+#Statement IF - ELIF - ELSE
+if (var_presiden == "Soekarno") :
+    print("Jawaban Anda Benar")
+elif(var_presiden == "Soeharto") :
+    print("Presiden Soeharto adalah presiden kedua")
+elif(var_presiden == "Jokowi") :
+    print ("President Jokowi adalah presiden saat ini")
+else :
+    print("Maaf, Jawaban Anda Salah")
+    
